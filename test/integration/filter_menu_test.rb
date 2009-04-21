d98af3ce51edc05f6bc7dbc8c3dfee0feb7d906d
Bryan Larsen <bryan@larsen.st>
2009-04-20 21:17:03 -0400

    blank integration test

---
+++ test/integration/filter_menu_test.rb
@@ -0,0 +1,10 @@
+require 'test_helper'
+
+class FilterMenuTest < ActionController::IntegrationTest
+  scenario "basic"
+
+  # Replace this with your real tests.
+  test "the truth" do
+    assert true
+  end
+end
