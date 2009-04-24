673711dfad43977795b9cb4065706a0a0541aba9
Bryan Larsen <bryan@larsen.st>
2009-04-24 17:34:57 -0400

    Run rake hobo:run_standard_generators

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 ActionController::Routing::Routes.draw do |map|
 
   map.site_search  'search', :controller => 'front', :action => 'search'
+  map.root :controller => 'front', :action => 'index'
+
+  map.site_search  'search', :controller => 'front', :action => 'search'
   map.homepage '', :controller => 'front', :action => 'index'
 
   Hobo.add_routes(map)
