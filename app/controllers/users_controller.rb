289e25656b6f3c2ddaff24b7ee96d6986fb3741d
Bryan Larsen <bryan@larsen.st>
2009-11-26 13:54:26 -0500

    update flash message because of bug fix #519

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,9 +7,11 @@ class UsersController < ApplicationController
   def do_signup
     hobo_do_signup do
       if this.errors.blank?
-        secret_path = user_activate_path :id=>this.id, :key => this.lifecycle.key
-        # FIXME: remove this line after you get email working reliably
+        flash[:notice] << "You must activate your account before you can log in.  Please check your email."
+        
+        # FIXME: remove these two lines after you get email working reliably
         # and before your application leaves its sandbox...
+        secret_path = user_activate_path :id=>this.id, :key => this.lifecycle.key
         flash[:notice] << "The 'secret' link that was just emailed was: <a id='activation-link' href='#{secret_path}'>#{secret_path}</a>."
       end
     end
