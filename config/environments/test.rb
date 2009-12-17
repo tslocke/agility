6ab56c497b571aed2a28d2ccbe0ec87a941f99ad
Bryan Larsen <bryan@larsen.st>
2009-12-16 23:05:51 -0600

    finally get this nested_scenarios thing right.

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -22,3 +22,5 @@ config.action_controller.allow_forgery_protection    = false
 config.action_mailer.delivery_method = :test
 
 config.gem "webrat"
+config.gem "josevalim-nested_scenarios", :lib => "nested_scenarios"
+
