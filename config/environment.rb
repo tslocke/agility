9dc0e2219762a8012accb08718c1fcc5ccfdeb92
Bryan Larsen <bryan@larsen.st>
2009-12-17 12:48:33 -0600

    jrubify

--- config/environment.rb
+++ config/environment.rb
@@ -11,7 +11,7 @@ RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
 require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
-  config.gem "bluecloth"
+  config.gem "BlueCloth", :lib => "bluecloth"
   config.gem "RedCloth", :lib => "redcloth"
   
   # Settings in config/environments/* take precedence over those specified here.
