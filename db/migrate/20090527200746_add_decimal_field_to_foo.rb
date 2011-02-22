d03a3c934fd17c514dd6343455850fcf2c893348
Bryan Larsen <bryan@larsen.st>
2011-02-21 23:02:49 -0500

    There's a bug in Rails 2.2.2 change_column on mysql, so add :scale to initial creation, not just to change_column.

--- db/migrate/20090527200746_add_decimal_field_to_foo.rb
+++ db/migrate/20090527200746_add_decimal_field_to_foo.rb
@@ -1,6 +1,6 @@
 class AddDecimalFieldToFoo < ActiveRecord::Migration
   def self.up
-    add_column :foos, :dec, :decimal
+    add_column :foos, :dec, :decimal, :scale =>4, :limit => nil, :precision => 10
   end
 
   def self.down
