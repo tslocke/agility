77bab6986aefd1c319d390ff66dc24f30499f9a7
Bryan Larsen <bryan@larsen.st>
2011-02-22 11:18:05 -0500

    collect gemsets on validation

--- validate-patches.sh
+++ validate-patches.sh
@@ -11,8 +11,11 @@ reset_patches
 # because of set -e, this will bail if git not clean
 git_cleanliness || (echo "git not clean, bailing" ; exit 1)
 
+rm -f gemset-list.txt
+
 for combo in ${combos[*]} ; do
     parse_combo $combo
+    echo $RUBY@$GEMSET >> gemset-list.txt
     patch_up
     patch_down
 done
